3cd01f0e9a7ee285cbbffb98d4cdf9d5229ca85a
hilmimuharromi <hilmimuharrom@Gmail.com>
2020-08-20 23:41:27 +0700

    setting router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './views/Home'
+import Pekerja from './views/Pekerja'
+const appRoutes = [
+  {
+    name: "Home",
+    component: Home,
+    exact: true,
+    path: "/",
+  },
+  {
+    name: "Pekerja",
+    component: Pekerja,
+    exact: true,
+    path: "/pekerja",
+  }
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {appRoutes.map((route) => (
+        <Route key={route.name} {...route} />
+      ))}
+    </Router>
   );
 }
 
